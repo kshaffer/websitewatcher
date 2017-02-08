8e8ce98d2519aec07a48f717f658c4581974df87
Kris Shaffer <kris.shaffer@gmail.com>
2017-02-08 09:12:10 -0500

    extract_url_database function

--- clean_url_list.py
+++ clean_url_list.py
@@ -1,6 +1,14 @@
 import json
 import csv
 
+"""
+def write_to_text(data, filename):
+    f = open(filename, 'w', encoding = 'utf-8')
+    for line in data:
+        f.write(line + '\n')
+    f.close()
+"""
+
 def read_text_file(filename):
     ignore = []
     ignore.append('[')
@@ -15,12 +23,6 @@ def read_text_file(filename):
                 table_original.append(line.rstrip(','))
     return table_original
 
-def write_to_text(data, filename):
-    f = open(filename, 'w', encoding = 'utf-8')
-    for line in data:
-        f.write(line + '\n')
-    f.close()
-
 def write_to_csv(data, filename):
     with open(filename, 'w') as csvfile:
         w = csv.writer(csvfile, delimiter = ',')
@@ -31,19 +33,17 @@ def write_to_csv(data, filename):
                 print(row, 'not written to file.')
     print(filename, 'created.')
 
-
-# import URLs from spider output
-u = read_text_file('whg.txt')
-
-# make into list
-url_list = [['url', 'timestamp', 'id']]
-for article in u:
-    url_object = []
-    url_object.append(json.loads(article)['file_url'])
-    url_object.append(json.loads(article)['timestamp'])
-    url_object.append(json.loads(article)['file_id'])
-    url_list.append(url_object)
+def extract_url_database(filename):
+    u = read_text_file(filename)
+    url_list = [['url', 'timestamp', 'id']]
+    for article in u:
+        url_object = []
+        url_object.append(json.loads(article)['file_url'])
+        url_object.append(json.loads(article)['timestamp'])
+        url_object.append(json.loads(article)['file_id'])
+        url_list.append(url_object)
+    return(url_list)
 
 # write list to text file, one URL per line
-write_to_csv(url_list, 'whg_url_data.csv')
+write_to_csv(extract_url_database('whg.txt'), 'whg_url_data.csv')
 
