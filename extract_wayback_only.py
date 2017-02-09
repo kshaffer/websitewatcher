87cbc7ec6ee0ce40b3fefa24dd84d217f87908b8
Kris Shaffer <kris.shaffer@gmail.com>
2017-02-09 16:03:54 -0500

    ed.gov stuff

---
+++ extract_wayback_only.py
@@ -0,0 +1,57 @@
+import json
+import csv
+
+def read_wayback_dump(filename):
+    """
+    reads and parses a text file of STDOUT from wayback_machine_downloader
+    returns a list of lines with valid JSON output
+    """
+    ignore = []
+    ignore.append('[')
+    ignore.append(']')
+    ignore.append('')
+    f = open(filename, encoding='utf-8')
+    table_original = []
+    for line in f:
+        line = line.rstrip('\n')
+        if line not in ignore:
+            if 'Getting snapshot pages' not in line:
+                table_original.append(line.rstrip(','))
+    return table_original
+
+def extract_wayback_url_database(wayback_data_dump):
+    """
+    extracts information from the results of read_wayback_dump
+    for each record in data, creates a dictionary record with the URL as key
+    and the timestamp of page as the value
+    """
+    db = []
+    for article in wayback_data_dump:
+        record_data = []
+        url = json.loads(article)['file_url']
+        timestamp = json.loads(article)['timestamp']
+        try:
+            url_split = url.split('.')
+            if len(url_split) > 3:
+                extension = url_split[len(url_split) - 1].split('?')[0]
+            else:
+                extension = 'html'
+        except:
+            extension = 'html'
+        record_data.append(url)
+        record_data.append(timestamp)
+        record_data.append(extension)
+        db.append(record_data)
+    return(db)
+
+def write_to_csv(data, filename):
+    with open(filename, 'w') as csvfile:
+        w = csv.writer(csvfile, delimiter = ',')
+        for row in data:
+            try:
+                w.writerow(row)
+            except:
+                print(row, 'not written to file.')
+    print(filename, 'created.')
+
+write_to_csv(extract_wayback_url_database(read_wayback_dump('test_data/ed_gov_wayback_2_9_2017.txt')), 'test_data/ed_gov_wayback_parsed.csv')
